3673d7d16d26cddc7cae1207ed05491cdfc43582
jjh946 <junhyeong.tech@gmail.com>
2022-04-25 14:49:32 +0900

    django 에러 해결 후
    rusnserver 가 안되고 django를 찾을 수 없다는 에러가 떴었는데 그 이유는 처음에는 새 폴더가 아닌 기존 사용하던 폴더에서 venv랑 쟝고를 설치해서 그렇고 내가 디렉토리를 이동시켜서 이다. 어찌 되었던 디렉토리 상태에 민감하게 반응한다.

--- Django/myvenv/Scripts/activate.bat
+++ Django/myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\조준형\Documents\GitHub\Django\myvenv
+set VIRTUAL_ENV=C:\Users\조준형\Documents\GitHub\likelion_study\Django\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
