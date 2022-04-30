e5fd0c7d3237272e6ee42688a9d66052ab414d77
jjh946 <junhyeong.tech@gmail.com>
2022-04-30 20:59:25 +0900

    6/14 쟝고를 쓰는 이유
    ㅇㄹ

--- Django/myproject/myapp/urls.py
+++ Django/myproject/myapp/urls.py
@@ -5,5 +5,5 @@ from myapp import views
 urlpatterns = [
   path('', views.index),
   path('create/', views.create),
-  path('read/1/', views.read)
+  path('read/<id>/', views.read)
 ]
--- Django/myproject/myapp/views.py
+++ Django/myproject/myapp/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render, HttpResponse
-
+import random
 # Create your views here.
 def index(request):
-    return HttpResponse('Welcome!!')
+    return HttpResponse('<h1>Random</h>' + str(random.random()))
 
 def create(request):
     return HttpResponse('CReate')
 
-def read(request):
-    return HttpResponse('REad!!')
+def read(request, id):
+    return HttpResponse('REad!!'+id)
