258e3c64333e2cd6ef864f6f56eca575ebb389c3
jjh946 <junhyeong.tech@gmail.com>
2022-04-30 20:52:18 +0900

    오 쟝고 라우팅 해봄
    create/
    read/1

--- Django/myproject/myapp/views.py
+++ Django/myproject/myapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Welcome!!')
+
+def create(request):
+    return HttpResponse('CReate')
+
+def read(request):
+    return HttpResponse('REad!!')
