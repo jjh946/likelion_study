72854b40a61935d972c31aec3af641ae48958fe5
jjh946 <junhyeong.tech@gmail.com>
2022-05-07 18:57:30 +0900

    **888888

--- python/codelion.py
+++ python/codelion.py
@@ -1,13 +1,7 @@
-information = {"고향":"수원", "취미":"영화관람","좋아하는 음식":"국수"}
+for x in range(30):
+    print(x)
+
 foods = ["된장찌개", "피자", "제육볶음"]
-print(information.get("취미"))
-information["특기"] = "피아노"
-information["사는곳"] = "서울"
-del information["좋아하는 음식"]
-print(information)
-print(len(information))
-information.clear()
-print(information)
-print(foods[-2])
-foods.append("김밥")
-print(foods)
+for x in range(3):
+    print(foods[x])
+    
