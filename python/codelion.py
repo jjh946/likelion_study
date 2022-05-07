747434bde13bcd8625ef594de1e42633cda85917
jjh946 <junhyeong.tech@gmail.com>
2022-05-07 18:41:25 +0900

    5555555

--- python/codelion.py
+++ python/codelion.py
@@ -1,6 +1,1 @@
-import random
-
-lunch = random.choice(["된장찌개","피자","제육볶음"])
-lunch = "냉장고"
-dinner = random.choice(["김밥","쫄면","돈까스"])
-print(lunch)
+information = {"고향":"서울","취미":"음악듣기", "좋아하는 음식":"갈비"}
