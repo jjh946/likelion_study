4900f1732602877ecda5e6b7f58f5773d26a021f
jjh946 <junhyeong.tech@gmail.com>
2022-05-07 18:30:41 +0900

    44444

--- python/codelion.py
+++ python/codelion.py
@@ -1,5 +1,9 @@
 import random
+import time
 
-for x in range(30):
-    print(random.choice(["된장찌개","제육볶음","치킨","떡볶이","라면"]))
-print("이 문장도 반복되나")
+while True:
+    
+    print(random.choice(["된장찌개","치킨","떡볶이","라면","감자튀김"]))
+    print("이 문장도 반복되나")
+    time.sleep(1)
+    break
