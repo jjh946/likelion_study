07ee41bf8e0eb8c54a5ee5b9d92f6549a15df4fd
jjh946 <junhyeong.tech@gmail.com>
2022-05-07 20:14:48 +0900

    10101010

--- python/codelion.py
+++ python/codelion.py
@@ -1,7 +1,12 @@
-for x in range(30):
-    print(x)
+lunch = ["된장찌개", "피자", "제육볶음", "짜장면"]
 
-foods = ["된장찌개", "피자", "제육볶음"]
-for x in range(3):
-    print(foods[x])
+while True:
+    print(lunch)
+    item = input("음식을 추가 해주세요 : ")
+    if(item == "q"):
+        break
+    else:
+        lunch.append(item)
     
+print(lunch)
+set_lunch = set(lunch)
