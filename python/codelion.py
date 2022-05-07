754b0e04dc2853b09439e427f6c77439c0eb5fde
jjh946 <junhyeong.tech@gmail.com>
2022-05-07 20:23:16 +0900

    Update codelion.py

--- python/codelion.py
+++ python/codelion.py
@@ -1,19 +1,6 @@
-def make_dolcelatte():
-    print("1. 얼음을 넣는다.")
-    print("2. 연유를 30ml 넣는다.")
-    print("3. 찬 우유를 넣는다.")
-    print("4. 에스프레소샷을 넣는다.")
+total_dictionary = {}
 
-def make_blueberry_smoothie():
-    print("1. 블루베리 20g을 넣는다.")
-    print("2. 우유 300ml를 넣는다.")
-    print("3. 얼음을 넣는다.")
-    print("4. 믹서기에 간다.")
-
-def make_simple_latte():
-    print("1. 커피를 넣는다.")
-    print("2. 우유를 넣는다.")
-    print("3. 신나게 섞는다.")
-
-make_simple_latte()
-make_blueberry_smoothie()
+while True:
+    question = input("질문을 입력해주세요 : ")
+    if question == "q":
+        break
