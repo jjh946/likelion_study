9e002a4f4ddbeb9f1b0a6582e2a58c63d7fb7826
jjh946 <junhyeong.tech@gmail.com>
2022-05-07 20:25:54 +0900

    Update codelion.py

--- python/codelion.py
+++ python/codelion.py
@@ -1,13 +1,14 @@
-total_dictionary = {}
+total_list = []
+
 while True:
     question = input("질문을 입력해주세요 : ")
     if question == "q":
         break
     else:
-        total_dictionary[question] = ""
+        total_list.append({"질문" : question, "답변" : ""})
 
-for i in total_dictionary:
-    print(i)
+for i in total_list:
+    print(i["질문"])
     answer = input("답변을 입력해주세요 : ")
     total_dictionary[i] = answer
 print(total_dictionary)
