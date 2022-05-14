2d0d39761ab99648d3c451522f824dd98ba7ffa2
jjh946 <junhyeong.tech@gmail.com>
2022-05-14 21:02:46 +0900

    Update codelion.py

--- python/codelion.py
+++ python/codelion.py
@@ -1,6 +1,8 @@
 import requests
+from bs4 import BeautifulSoup
 
-url = "http://www.icampus.skku.edu"
+url = "http://www.daum.net/"
 response = requests.get(url)
+# print(response.text)
 
-print(response.text)
+print(BeautifulSoup(response.text, 'html.parser'))
