7fe0885699fac383de6716ea4ae8ae2a8e943ae4
jjh946 <junhyeong.tech@gmail.com>
2022-05-14 20:15:47 +0900

    심화 스타트

--- Django/myproject/myapp/views.py
+++ Django/myproject/myapp/views.py
@@ -39,7 +39,26 @@ def index(request):
     return HttpResponse(HTMLTemplate(article))
 
 def create(request):
-    return HttpResponse('CReate')
+	global nextId
+	if request.method == 'GET':  #get방식과 post방식으로 들어왔을때의 처리를 다르게 해준다
+		article = '''
+			<form action="/create/" method="post"> #포스트 방식으로 바꿔준다
+				<p><input type="text" name="title" placeholder="title"></p> <!--글을 입력할 수 있는 칸을 만들고 title로 이름, placeholder로 보이는 글자를 생성-->
+				<p><textarea name="body" placeholder="body"></textarea></p> <!--text area로 여러 줄의 text를 입력-->
+				<p><input type="submit"></p>
+			</form>  <!--데이터를 원하는 패쓰로 보내주기위해서는 폼태그로 감싸줘야 한다-->
+		'''
+    return HttpResponse(HTMLTemplate(article))
+    
+	elif request.method == 'POST'
+		title = request.POST['title'] #request.POST로 들어온 값에 이름을 붙여준다
+		body = request.POST['body']
+		newTopic = {"id":nextId, "title":title, "body":body #위쪽에 있는 토픽에다 추가해 줘야 한다 nextId
+		topics.append(newTopic) #새로운 토픽을 기존의 톡픽들에 추가해준다
+		url = '/read/'+str(nextId) #nextId 문자열로 바꿔줘야 잘 추가됨
+		nextId = n
+extId + 1  #계속 id가 4가 아닌 다음 수가 되게 해준다
+		return redirect(url) #의미없는 값을 반환해 주는 것이 아닌 nextId가 갱신되기 이전의 url로 이동해 준다
 
 def read(request, id):
     global topics
--- python/codelion.py
+++ python/codelion.py
@@ -1,14 +1,3 @@
-total_list = []
-while True:
-    question = input("질문을 입력해주세요 : ")
-    if question == "q":
-        break
-    else:
-        total_list.append({"질문" : question, "답변" : ""})
+import requests
 
-
-for i in total_list:
-    print(i["질문"])
-    answer = input("답변을 입력해주세요 : ")
-    i["답변"] = answer
-print(total_list)
+print(requests.get)
