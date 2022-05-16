145e6553b16a5c253ec34f02acd362ba0ce00984
jjh946 <junhyeong.tech@gmail.com>
2022-05-16 11:45:19 +0900

    Update codelion.py

--- python/codelion.py
+++ python/codelion.py
@@ -3,6 +3,6 @@ from bs4 import BeautifulSoup
 
 url = "http://www.daum.net/"
 response = requests.get(url)
-# print(response.text)
+print(type(response.text))
 
-print(BeautifulSoup(response.text, 'html.parser'))
+print(type(BeautifulSoup(response.text, 'html.parser')))
