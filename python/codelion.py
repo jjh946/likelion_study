2f89bb9ce95c7583a36525a6b858287940ae66f2
jjh946 <junhyeong.tech@gmail.com>
2022-05-17 11:46:07 +0900

    Update codelion.py

--- python/codelion.py
+++ python/codelion.py
@@ -9,8 +9,11 @@ rank = 1
 
 results = soup.findAll('a','link_favorsch')
 
-print(datetime.today())
+search_rank_file = open("rankresult.txt","w")
+
+print(datetime.today().strftime("%Y년 %m월 %d일의 실시간 검색어 순위입니다.\n"))
 
 for result in results:
+    search_rank_file.write(str(rank)+"위:"+result.get_text()+"\n")
     print(rank,"위 : ",result.get_text(),"\n")
     rank += 1
