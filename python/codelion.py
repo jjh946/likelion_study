d5a2201a09c1fcbd025c4ff4ab7e2dfd8459d5c6
jjh946 <junhyeong.tech@gmail.com>
2022-05-18 17:10:22 +0900

    Update codelion.py

--- python/codelion.py
+++ python/codelion.py
@@ -1,19 +1,8 @@
-from bs4 import BeautifulSoup
 import requests
-from datetime import datetime
 
-url = "http://www.daum.net/"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-rank = 1
+city = "Seoul"
+apikey = "8d32d32a2c9cd8743896215a8f38a513"
+api = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}"
 
-results = soup.findAll('a','link_favorsch')
-
-search_rank_file = open("rankresult.txt","a")
-
-print(datetime.today().strftime("%Y년 %m월 %d일의 실시간 검색어 순위입니다.\n"))
-
-for result in results:
-    search_rank_file.write(str(rank)+"위:"+result.get_text()+"\n")
-    print(rank,"위 : ",result.get_text(),"\n")
-    rank += 1
+result = requests.get(api)
+print(result.text)
