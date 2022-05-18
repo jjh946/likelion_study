f047b2bb50be909c21f48d48ba5b24009df98372
jjh946 <junhyeong.tech@gmail.com>
2022-05-18 17:39:19 +0900

    Update codelion.py

--- python/codelion.py
+++ python/codelion.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 city = "Seoul"
 apikey = "8d32d32a2c9cd8743896215a8f38a513"
@@ -6,3 +7,12 @@ api = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}"
 
 result = requests.get(api)
 print(result.text)
+
+data = json.loads(result.text)
+
+# print(type(result.text))
+# print(type(data))
+
+print(data["name"],"의 날씨입니다.")
+print(data["weather"])
+print("Today is ",data["weather"][0]["main"])
