6050d34be208f4194fa5502cac7900472dfbfa83
jjh946 <junhyeong.tech@gmail.com>
2022-05-24 21:21:18 +0900

    갯 포스트 매소드 설정

--- Django/myproject/myapp/views.py
+++ Django/myproject/myapp/views.py
@@ -1,10 +1,11 @@
 from ast import Return
+import re
 from django.http import HttpResponse
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 import random
+from django.views.decorators.csrf import csrf_exempt
 
-
-
+nextId = 4
 topics = [
     {'id':1, 'title':'routing', 'body':'Routing is ..'},
     {'id':2, 'title':'view', 'body':'View is ..'},
@@ -47,12 +48,24 @@ def read(request, id):
             article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
     return HttpResponse(HTMLTemplate(article))
 
+@csrf_exempt
 def create(request):
-    article = '''
-    	<form action="/create/" method="post"> 
-				<p><input type="text" name="title" placeholder="title"></p>
-				<p><textarea name="body" placeholder="body"></textarea></p>
-				<p><input type="submit"></p>
-			</form>
-    '''
-    return HttpResponse(HTMLTemplate(article))
+    global nextId
+    print('request.method', request.method)
+    if request.method == 'GET':
+        article = '''
+            <form action="/create/" method="post"> 
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="submit"></p>
+            </form>
+        '''
+        return HttpResponse(HTMLTemplate(article))
+    elif request.method == 'POST':
+        title = request.POST['title']
+        body = request.POST['body'] 
+        newTopic = {'id':nextId, "title":title, 'body':body}       
+        topics.append(newTopic)
+        url = '/read/'+nextId
+        nextId = nextId + 1
+        return redirect(url)
