abe670f1c1c6aa887ce4d0875531fd34fb91f2af
jjh946 <junhyeong.tech@gmail.com>
2022-05-26 14:45:22 +0900

    13 update 기능 완료

--- Django/myproject/myapp/urls.py
+++ Django/myproject/myapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
   path('', views.index),
   path('create/', views.create),
   path('read/<id>/', views.read),
+  path('update/<id>/', views.update),
   path('delete/', views.delete)
 ]
